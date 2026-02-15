1e2f41cb990513778a729dc6e7e182952a641d68
Tobey Codes <tobey@tobey.codes>
2026-02-15 16:59:39 +0700

    feat: add tui

--- cspell.config.ts
+++ cspell.config.ts
@@ -2,7 +2,7 @@ import type { CSpellSettings } from "cspell";
 
 const config: CSpellSettings = {
 	useGitignore: true,
-	words: ["syncpack", "turborepo", "shadcn", "fontsource"],
+	words: ["syncpack", "turborepo", "shadcn", "fontsource", "opentui"],
 };
 
 export default config;
